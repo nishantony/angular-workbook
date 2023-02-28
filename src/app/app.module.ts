c4af73e736443e183b26374458a8c9ea3e241cbc
nishantony <39977811+nishantony@users.noreply.github.com>
2023-02-28 15:24:58 +0100

    adjusted spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 import { SampleReactiveFormsComponent } from './components/sample-reactive-forms/sample-reactive-forms.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReactiveFormsComponent,
-    SampleReactiveFormsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, SampleReactiveFormsComponent],
+    imports: [BrowserModule, ReactiveFormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
 
--- src/app/components/sample-reactive-forms/sample-reactive-forms.component.ts
+++ src/app/components/sample-reactive-forms/sample-reactive-forms.component.ts
@@ -1,39 +1,39 @@
 import { Component } from '@angular/core';
 import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
 } from '@angular/forms';
 
 @Component({
-  selector: 'app-sample-reactive-forms',
-  templateUrl: './sample-reactive-forms.component.html',
-  styleUrls: ['./sample-reactive-forms.component.css'],
+    selector: 'app-sample-reactive-forms',
+    templateUrl: './sample-reactive-forms.component.html',
+    styleUrls: ['./sample-reactive-forms.component.css'],
 })
 export class SampleReactiveFormsComponent {
-  signupForm: FormGroup;
+    signupForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-    this.signupForm = new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-    });
-  }
+    ngOnInit() {
+        this.signupForm = new FormGroup({
+            firstName: new FormControl('', [Validators.required]),
+            lastName: new FormControl('', [Validators.required]),
+            email: new FormControl('', [Validators.required, Validators.email]),
+        });
+    }
 
-  get signupControls() {
-    return this.signupForm.controls;
-  }
+    get signupControls() {
+        return this.signupForm.controls;
+    }
 
-  onSubmit() {
-    let data = {
-      fname: this.signupForm.value.firstName,
-      lname: this.signupForm.value.lastName,
-      email: this.signupForm.value.email,
-    };
-    console.log(data);
-  }
+    onSubmit() {
+        let data = {
+            fname: this.signupForm.value.firstName,
+            lname: this.signupForm.value.lastName,
+            email: this.signupForm.value.email,
+        };
+        console.log(data);
+    }
 }
